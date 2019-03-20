4b45d4c490a91d4ddfa60c1bd881c4b7c8aae876
root <root@localhost.localdomain>
2019-03-20 14:12:36 +0200

    20.03.2019

--- usermanager.sh
+++ usermanager.sh
@@ -7,10 +7,11 @@ while [ $var == false ]
 do
 clear;
 echo -e "\e[32m==============\e[0m\e[31mUSER EDIT\e[0m\e[32m===============\e[0m"
-echo -e "\n\e[32mNAME :: 1\e[0m";
-echo -e "\n\e[32mUID :: 2\e[0m";
-echo -e "\n\e[32mGID :: 3\e[0m";
-echo -e "\n\e[32mEXIT :: 0\e[0m";
+echo -e "\n\e[32mNAME \e[0m\e[31m:: 1\e[0m";
+echo -e "\n\e[32mUID \e[0m\e[31m:: 2\e[0m";
+echo -e "\n\e[32mGID \e[0m\e[31m:: 3\e[0m";
+echo -e "\n\e[32mChnge password \e[0m\e[31m:: 4\e[0m";
+echo -e "\n\e[31mEXIT \e[0m\e[32m:: 0\e[0m";
 read choise;
 case $choise in
 1)
@@ -24,21 +25,34 @@ break;
 fi
 echo -e "\e[37mEnter new name \e[0m";
 read newname;
-usermod name -l newname;
+usermod $name -l $newname;
 ;;
 2) 
-echo -e "\e[37mEnter uid \e[0m";
-read uid;
+echo -e "\e[37mEnter name for change uid \e[0m";
+read name;
 echo -e "\e[37mEnter newuid \e[0m";
 read newuid;
-usermod uid -u newuid;
+usermod $name -u $newuid;
 ;;
 3) 
-echo -e "\e[37mEnter group \e[0m";
-read group;
-echo -e "\e[37mEnter name \e[0m";
+echo -e "\e[37mEnter name for change group \e[0m";
+read name;
+echo -e "\e[37mEnter newgroup \e[0m";
 read newgroup;
-usermod group -g newgroup;
+usermod $name -g $newgroup;
+;;
+4) 
+	echo -e "\e[31mCHANGE PASSWORD!!!\e[0m"
+	echo -e "\e[32mEnter username: \e[0m"
+     	read newUserName
+     	cat /etc/passwd | grep $newUserName
+	if (( $? == 0 ))
+ 	then
+      	 passwd $newUserName;
+	else
+	echo -e "\e[31mEROR!!!\e[0m";
+	/bin/sleep 1; 
+	fi
 ;;
 0)let var=true ;;
 *)echo -e "\e[31mEROR!!![0m";
@@ -54,8 +68,8 @@ while [ $var == false ]
 do
 clear;
 echo -e "\e[32m==============\e[0m\e[31mUSER ADD\e[0m\e[32m===============\e[0m"
-echo -e "\n\e[32mBY UID :: 1\e[0m";
-echo -e "\n\e[32mEXIT :: 0\e[0m";
+echo -e "\n\e[32mBY UID \e[0m\e[31m:: 1\e[0m";
+echo -e "\n\e[32mEXIT \e[0m\e[31m:: 0\e[0m";
 read choise;
 
 case $choise in 
@@ -88,11 +102,11 @@ while [ $var == false ]
 do
 clear;
 echo -e "\e[32m==============\e[0m\e[31mUSER ADD\e[0m\e[32m===============\e[0m"
-echo -e "\n\e[36m1\e[0m\e[31m-BY NAME\e[0m";
-echo -e "\n\e[36m2\e[0m\e[31m-BY UID\e[0m";
-echo -e "\n\e[36m3\e[0m\e[31m-BY GID\e[0m";
-echo -e "\n\e[36m4\e[0m\e[31m-Check Users\e[0m";
-echo -e "\n\e[36m5\e[0m\e[31m-EXIT\e[0m";
+echo -e "\n\e[32m1\e[0m\e[31m-BY NAME\e[0m";
+echo -e "\n\e[32m2\e[0m\e[31m-BY UID\e[0m";
+echo -e "\n\e[32m3\e[0m\e[31m-BY GID\e[0m";
+echo -e "\n\e[32m4\e[0m\e[31m-Check Users\e[0m";
+echo -e "\n\e[32m5\e[0m\e[31m-EXIT\e[0m";
 read choise;
 case $choise in
 1)	
@@ -134,11 +148,11 @@ varib=true;
 while [ $varib == true ]
 do
 clear;
-echo -e "\e[33mCreate user - 1 \e[0m";
-echo -e "\e[33mDell user - 2 :: \e[0m";
-echo -e "\e[33mEdit user - 3 :: \e[0m";
-echo -e "\e[33mCheck user - 4 :: \e[0m";
-echo -e "\e[33mEXIT - 0 :: \e[0m";
+echo -e "\e[32mCreate user \e[0m\e[31m- 1 :: \e[0m";
+echo -e "\e[32mDell user   \e[0m\e[31m- 2 :: \e[0m";
+echo -e "\e[32mEdit user   \e[0m\e[31m- 3 :: \e[0m";
+echo -e "\e[32mCheck user  \e[0m\e[31m- 4 :: \e[0m";
+echo -e "\e[31mEXIT - 0 :: \e[0m";
 read choice;
 case $choice in
 1)
@@ -152,18 +166,18 @@ userEDIT;
 ;;
 4)
 clear
-less /etc/shadow
-/bin/sleep 1;
+cat /etc/passwd;
+/bin/sleep 4;
 ;;
 0)
-echo -e "\e[34mGOODD\e[0m\e[33mBYE\e[0m ";
+echo -e "\e[34mGOOD\e[0m\e[31mBYE\e[0m";
 let varib=false;
 /bin/sleep 1;
 ;; 
 *)
 clear;
  echo -e "\e[31mERROR\e[0m"; 
-/bin/sleep 3;
+/bin/sleep 2;
 ;;
 esac
 
